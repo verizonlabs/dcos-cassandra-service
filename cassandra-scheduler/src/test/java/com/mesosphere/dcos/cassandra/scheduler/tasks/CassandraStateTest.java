32435dc24feab6e0f590a086c8b731627781a615
Aaron Wood <aaronjwood@gmail.com>
2017-02-20 18:33:12 -0500

    Fix tests

--- cassandra-scheduler/src/test/java/com/mesosphere/dcos/cassandra/scheduler/tasks/CassandraStateTest.java
+++ cassandra-scheduler/src/test/java/com/mesosphere/dcos/cassandra/scheduler/tasks/CassandraStateTest.java
@@ -193,9 +193,9 @@ public class CassandraStateTest {
     private void validateDaemonTaskInfo(Protos.TaskInfo daemonTaskInfo) throws TaskException {
         Assert.assertEquals(testDaemonName, daemonTaskInfo.getName());
         if (daemonTaskInfo.getContainer().getVolumesList().isEmpty()){
-            Assert.assertEquals(4, daemonTaskInfo.getResourcesCount());
+            Assert.assertEquals(3, daemonTaskInfo.getResourcesCount());
         } else {
-            Assert.assertEquals(4, daemonTaskInfo.getResourcesCount());
+            Assert.assertEquals(3, daemonTaskInfo.getResourcesCount());
         }
         Assert.assertEquals(testDaemonName, TaskUtils.toTaskName(daemonTaskInfo.getTaskId()));
         Assert.assertTrue(daemonTaskInfo.getSlaveId().getValue().isEmpty());
